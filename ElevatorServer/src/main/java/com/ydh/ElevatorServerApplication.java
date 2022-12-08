3ce0976f6f687c645ea154f64704d9ef620de9b4
ydh0520 <ydh2338@naver.com>
2022-12-09 01:44:57 +0900

    Srping init

---
+++ ElevatorServer/src/main/java/com/ydh/ElevatorServerApplication.java
@@ -0,0 +1,13 @@
+package com.ydh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorServerApplication.class, args);
+	}
+
+}
---
+++ ElevatorServer/src/test/java/com/ydh/ElevatorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
